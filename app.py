37c15148ecb323f4fab60f979450be3d330e3e83
prahalad.an <prahaladanand6@gmail.com>
2024-02-27 22:34:40 -0600

    added base jinja templating

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, sqlalchemy
 
 app = Flask(__name__)
 
+app.debug = True
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+if  __name__ == '__main__':
+    app.run(debug=True)
